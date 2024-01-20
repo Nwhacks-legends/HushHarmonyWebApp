519cfa86366f3b897115205c64cd3daa507e38cf
katielaptop <katiesun138@gmail.com>
2024-01-20 13:26:01 -0800

    two pages and routes added 〠

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import MappedInPage from './MappedInPage';
+
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Link to="/">Basic</Link>
+    <Routes>
+      <Route path="/" element={<MappedInPage />} />
+    </Routes>
+  </BrowserRouter>
+    
+
   );
 }
 
---
+++ src/MappedInPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const MappedInPage = ()=>{
+    return (
+        <div>
+          <h1>This is the New Page</h1>
+          <p>Welcome to the new page!</p>
+        </div>
+      );
+    
+}
+
+export default MappedInPage;
